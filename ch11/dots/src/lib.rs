e9b3c1b9cbf192dc6972aa271a48aa684d8a597d
cmilanesi <carlo.milanesi@libero.it>
2020-04-27 00:37:49 +0200

    Simplified dots.

--- ch11/dots/src/lib.rs
+++ ch11/dots/src/lib.rs
@@ -1,43 +1,42 @@
+/*
 #![no_std]
 
 extern crate alloc;
 
 use crate::alloc::boxed::Box;
-use linux_kernel_module::bindings;
+use linux_kernel_module::bindings::{
+    __register_chrdev, __unregister_chrdev, _copy_to_user, file, file_operations, loff_t,
+};
 use linux_kernel_module::c_types;
 use linux_kernel_module::println;
-
-use linux_kernel_module::bindings::__register_chrdev;
-use linux_kernel_module::bindings::__unregister_chrdev;
-use linux_kernel_module::bindings::_copy_to_user;
 
 struct CharDeviceGlobalData {
     major: c_types::c_uint,
     name: &'static str,
-    fops: Box<bindings::file_operations>,
+    fops: Box<file_operations>,
     count: u64,
 }
 
 static mut GLOBAL: Option<CharDeviceGlobalData> = None;
 
 extern "C" fn read_dot(
-    _arg1: *mut bindings::file,
+    _arg1: *mut file,
     arg2: *mut c_types::c_char,
     _arg3: usize,
-    _arg4: *mut bindings::loff_t,
+    _arg4: *mut loff_t,
 ) -> isize {
     unsafe {
         if let Some(global) = &mut GLOBAL {
             global.count += 1;
             _copy_to_user(
                 arg2 as *mut c_types::c_void,
-                if global.count % 10 == 0 { "*" } else { "." }
-                    .as_ptr() as *const c_types::c_void,
+                if global.count % 10 == 0 { "*" } else { "." }.as_ptr() as *const c_types::c_void,
                 1,
             );
             1
+        } else {
+            0
         }
-        else { 0 }
     }
 }
 
@@ -46,7 +45,7 @@ pub extern "C" fn init_module() -> c_types::c_int {
     let mut data = CharDeviceGlobalData {
         major: 0,
         name: "dots\0",
-        fops: Box::new(bindings::file_operations::default()),
+        fops: Box::new(file_operations::default()),
         count: 0,
     };
     data.fops.read = Some(read_dot);
@@ -59,7 +58,9 @@ pub extern "C" fn init_module() -> c_types::c_int {
             &*data.fops,
         )
     };
-    if major < 0 { return 1; }
+    if major < 0 {
+        return 1;
+    }
     data.major = major as c_types::c_uint;
     println!("dots: Loaded with major device number {}", major);
     unsafe {
@@ -85,4 +86,87 @@ pub extern "C" fn cleanup_module() {
 
 #[link_section = ".modinfo"]
 pub static MODINFO: [u8; 12] = *b"license=GPL\0";
+*/
+#![no_std]
 
+extern crate alloc;
+
+use crate::alloc::boxed::Box;
+use linux_kernel_module::bindings::{
+    __register_chrdev, __unregister_chrdev, _copy_to_user, file, file_operations, loff_t,
+};
+use linux_kernel_module::c_types;
+use linux_kernel_module::println;
+
+struct CharDeviceGlobalData {
+    major: c_types::c_uint,
+    name: &'static str,
+    fops: Option<Box<file_operations>>,
+    count: u64,
+}
+
+static mut GLOBAL: CharDeviceGlobalData = CharDeviceGlobalData {
+    major: 0,
+    name: "dots\0",
+    fops: None,
+    count: 0,
+};
+
+extern "C" fn read_dot(
+    _arg1: *mut file,
+    arg2: *mut c_types::c_char,
+    _arg3: usize,
+    _arg4: *mut loff_t,
+) -> isize {
+    unsafe {
+        GLOBAL.count += 1;
+        _copy_to_user(
+            arg2 as *mut c_types::c_void,
+            if GLOBAL.count % 10 == 0 { "*" } else { "." }.as_ptr() as *const c_types::c_void,
+            1,
+        );
+        1
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn init_module() -> c_types::c_int {
+    let mut fops = Box::new(file_operations::default());
+    fops.read = Some(read_dot);
+    let major = unsafe {
+        __register_chrdev(
+            0,
+            0,
+            256,
+            GLOBAL.name.as_bytes().as_ptr() as *const i8,
+            &*fops,
+        )
+    };
+    if major < 0 {
+        return 1;
+    }
+    unsafe {
+        GLOBAL.major = major as c_types::c_uint;
+    }
+    println!("dots: Loaded with major device number {}", major);
+    unsafe {
+        GLOBAL.fops = Some(fops);
+    }
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn cleanup_module() {
+    unsafe {
+        println!("dots: Unloaded {}", GLOBAL.count);
+        __unregister_chrdev(
+            GLOBAL.major,
+            0,
+            256,
+            GLOBAL.name.as_bytes().as_ptr() as *const i8,
+        )
+    }
+}
+
+#[link_section = ".modinfo"]
+pub static MODINFO: [u8; 12] = *b"license=GPL\0";
