5f8c9b09c788bb8e0d5d386d40fdaae629c87bb7
cmilanesi <carlo.milanesi@libero.it>
2020-04-27 04:00:42 +0200

    Removed commented-out code

--- ch11/dots/src/lib.rs
+++ ch11/dots/src/lib.rs
@@ -1,92 +1,3 @@
-/*
-#![no_std]
-
-extern crate alloc;
-
-use crate::alloc::boxed::Box;
-use linux_kernel_module::bindings::{
-    __register_chrdev, __unregister_chrdev, _copy_to_user, file, file_operations, loff_t,
-};
-use linux_kernel_module::c_types;
-use linux_kernel_module::println;
-
-struct CharDeviceGlobalData {
-    major: c_types::c_uint,
-    name: &'static str,
-    fops: Box<file_operations>,
-    count: u64,
-}
-
-static mut GLOBAL: Option<CharDeviceGlobalData> = None;
-
-extern "C" fn read_dot(
-    _arg1: *mut file,
-    arg2: *mut c_types::c_char,
-    _arg3: usize,
-    _arg4: *mut loff_t,
-) -> isize {
-    unsafe {
-        if let Some(global) = &mut GLOBAL {
-            global.count += 1;
-            _copy_to_user(
-                arg2 as *mut c_types::c_void,
-                if global.count % 10 == 0 { "*" } else { "." }.as_ptr() as *const c_types::c_void,
-                1,
-            );
-            1
-        } else {
-            0
-        }
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn init_module() -> c_types::c_int {
-    let mut data = CharDeviceGlobalData {
-        major: 0,
-        name: "dots\0",
-        fops: Box::new(file_operations::default()),
-        count: 0,
-    };
-    data.fops.read = Some(read_dot);
-    let major = unsafe {
-        __register_chrdev(
-            0,
-            0,
-            256,
-            data.name.as_bytes().as_ptr() as *const i8,
-            &*data.fops,
-        )
-    };
-    if major < 0 {
-        return 1;
-    }
-    data.major = major as c_types::c_uint;
-    println!("dots: Loaded with major device number {}", major);
-    unsafe {
-        GLOBAL = Some(data);
-    }
-    0
-}
-
-#[no_mangle]
-pub extern "C" fn cleanup_module() {
-    unsafe {
-        if let Some(global) = &GLOBAL {
-            println!("dots: Unloaded {}", global.count);
-            __unregister_chrdev(
-                global.major,
-                0,
-                256,
-                global.name.as_bytes().as_ptr() as *const i8,
-            )
-        }
-    }
-}
-
-#[link_section = ".modinfo"]
-pub static MODINFO: [u8; 12] = *b"license=GPL\0";
-*/
 #![no_std]
 
 extern crate alloc;
@@ -111,23 +22,6 @@ static mut GLOBAL: CharDeviceGlobalData = CharDeviceGlobalData {
     fops: None,
     count: 0,
 };
-
-extern "C" fn read_dot(
-    _arg1: *mut file,
-    arg2: *mut c_types::c_char,
-    _arg3: usize,
-    _arg4: *mut loff_t,
-) -> isize {
-    unsafe {
-        GLOBAL.count += 1;
-        _copy_to_user(
-            arg2 as *mut c_types::c_void,
-            if GLOBAL.count % 10 == 0 { "*" } else { "." }.as_ptr() as *const c_types::c_void,
-            1,
-        );
-        1
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn init_module() -> c_types::c_int {
@@ -165,6 +59,23 @@ pub extern "C" fn cleanup_module() {
             256,
             GLOBAL.name.as_bytes().as_ptr() as *const i8,
         )
+    }
+}
+
+extern "C" fn read_dot(
+    _arg1: *mut file,
+    arg2: *mut c_types::c_char,
+    _arg3: usize,
+    _arg4: *mut loff_t,
+) -> isize {
+    unsafe {
+        GLOBAL.count += 1;
+        _copy_to_user(
+            arg2 as *mut c_types::c_void,
+            if GLOBAL.count % 10 == 0 { "*" } else { "." }.as_ptr() as *const c_types::c_void,
+            1,
+        );
+        1
     }
 }
 
